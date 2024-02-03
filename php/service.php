0c6d0af16b871acb615e3a863c575ac8f40df739
Tizedit <comunicacion@tizedit.com>
2024-02-03 13:02:44 +0100

    FC

---
+++ php/service.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * PHP as a service - retreiving data with fetch API
+ * @author Tizedit - David G. bonacho
+ */
+
+// retreive params (they come as Querystring)
+$params = $_POST;
+
+// your code here
+
+
+// your response
+$response = $params;
+
+// convert to JSON
+$encoded = json_encode($response, JSON_PRETTY_PRINT); // , JSON_FORCE_OBJECT   , JSON_PRETTY_PRINT
+header('Content-type: application/json');
+
+exit($encoded);
